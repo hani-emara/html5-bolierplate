37a87b1df2ac825d5e6c9e746862a53be4f4a739
Hani Mahmoud <1hanymahmoud@gmail.com>
2019-01-14 01:11:41 +0200

    finished webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,23 @@
 const path = require('path');
-const webpack = require('webpack')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackMd5Hash = require("webpack-md5-hash");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-// const nodeExternals = require("webpack-node-externals");
  
 
 module.exports = {
-
     entry: "./src/index.js",
-
     output: {
-
         path: path.resolve(__dirname, 'dist'),
-
         filename: "[name].[chunkhash].js"
-
     },
-
     optimization: {
         splitChunks: {
-
             chunks: "all",
-
         }
     },
-
     module: {
-
         rules: [
-
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
@@ -41,56 +28,49 @@ module.exports = {
                     }
                 }
             },
-
             {
-
                 test: /\.s?css$/,
-
-                use: ["style-loader",MiniCssExtractPlugin.loader , "css-loader","postcss-loader", "sass-loader"]
-
+                use: ["style-loader",MiniCssExtractPlugin.loader , "css-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            plugins: [
+                                require("autoprefixer")()
+                            ],
+                        }
+                    },
+                 "sass-loader"]
             },
-
             {
-
-                test: /\.(png|jpg|gif)$/,
-
+                test: /\.(jpe?g|png|gif|svg|ico)$/i,
                 use: [
-                    "file-loader", {
-                   
+                    "file-loader", {          
                         loader: "url-loader",
-
                         options: {
                             limit: 5000
                         }
-                        
                     }
-
                 ]
-
-            }
-
+            },
+            {
+                test: /\.(ttf|eot|woff|woff2)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: "fonts/[name].[ext]",
+                    },
+                },
+            },
         ]
-
     },
-
     plugins: [
-
         new HtmlWebpackPlugin({
-            // inject: false,
             hash: true,
             template: './src/index.html',
             filename: 'index.html'
         }),
-
         new MiniCssExtractPlugin(),
-
         new CleanWebpackPlugin('dist', {}),
-
         new WebpackMd5Hash(),
-
-        // new webpack.HotModuleReplacementPlugin()
-
-
     ]
-
 }
